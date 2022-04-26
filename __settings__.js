ecc6cc8668441a25d20ba426859f4a99d5bacdbc
Muhammad Saad Jumani <saadjumani@hotmail.com>
2022-04-26 10:47:05 +0500

    Most of the unity functionality transferred. Scaling remains.

--- __settings__.js
+++ __settings__.js
@@ -8,7 +8,7 @@ CONTEXT_OPTIONS = {
     'preferWebGl2': true,
     'powerPreference': "default"
 };
-SCRIPTS = [ 77854887, 78097772 ];
+SCRIPTS = [ 77854887, 78097772, 78382718, 79014545, 79153391, 79459251, 79465795, 79469394 ];
 CONFIG_FILENAME = "config.json";
 INPUT_SETTINGS = {
     useKeyboard: true,
@@ -18,4 +18,5 @@ INPUT_SETTINGS = {
 };
 pc.script.legacy = false;
 PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/78379718/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/78379719/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/78379717/1/ammo.js', 'preload' : true},
 ];
